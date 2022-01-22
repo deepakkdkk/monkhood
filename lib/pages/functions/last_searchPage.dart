42880d29503f016efd1d775fe62d51f8388b7841
deepakkdkk <deepakkumardk2811@gmail.com>
2022-01-22 16:31:13 +0530

    last_searchPage style changed

--- lib/pages/functions/last_searchPage.dart
+++ lib/pages/functions/last_searchPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'full_screenPhoto.dart';
+
 class LastSearch extends StatefulWidget {
   final String rate;
   final String photo;
@@ -14,12 +15,17 @@ class _LastSearchState extends State<LastSearch> {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: GestureDetector(
-        onTap: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => LastSearchTap(widget.photo),),);
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => LastSearchTap(widget.photo),
+            ),
+          );
         },
         child: Container(
-          height: 175,
-          width: 150,
+          height: 200,
+          width: 175,
           decoration: new BoxDecoration(
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(20),
@@ -34,21 +40,80 @@ class _LastSearchState extends State<LastSearch> {
           ),
           child: Column(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(widget.rate),
-                  GestureDetector(
-                    child: Icon(
-                      Icons.fullscreen,
-                    ),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => FullScreenPhoto(widget.photo)),
-                      );
-                    },
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          
+                          color: Color(0xFF345C5F),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(widget.rate + "/- pm",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),),
+                        ),
+                      ),
+                      GestureDetector(
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Image(
+                                
+                                height: 10.0,
+                                width: 10.0,
+                                image: AssetImage('assets/full_imageIcon.png'),
+                              ),
+                            ),
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    FullScreenPhoto(widget.photo)),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 100,),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Text('Block B',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat',
+                      fontSize: 15,
+                    ),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left:10.0),
+                    child: Text('Sector 17,Rohini',
+                    style: TextStyle(
+                      color: Colors.white,
+                      
+                      fontFamily: 'Montserrat',
+                      fontSize: 15,
+                    ),),
                   ),
                 ],
               )
--- lib/pages/functions/search_bar.dart
+++ lib/pages/functions/search_bar.dart
@@ -13,14 +13,15 @@ class _SearchBarState extends State<SearchBar> {
       child: Row(
         children: [
           Expanded(
+            flex: 5,
             child: Padding(
-              padding: const EdgeInsets.fromLTRB(10, 10, 25, 10),
+              padding: const EdgeInsets.fromLTRB(10, 13, 10, 13),
               child: TextField(
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: Color(0xFFF2F2F2),
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25),
+                    borderRadius: BorderRadius.circular(45),
                   ),
                   prefixIcon: FlatButton(
                     child: Icon(
@@ -28,13 +29,33 @@ class _SearchBarState extends State<SearchBar> {
                     ),
                     onPressed: () {},
                   ),
-                  suffixIcon: Icon(
-                    Icons.filter_3_rounded,
-                  ),
+                  // suffixIcon: Image(
+                  //   height: 10.0,
+                  //   width: 10.0,
+                  //   image: AssetImage('assets/filter_icon.png'),
+                  // ),
                   hintText: 'Enter the place name',
                   hintStyle: TextStyle(
                     color: Colors.grey,
                   ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 13, 10, 13),
+              decoration: BoxDecoration(
+                color: Color(0xFF345C5F),
+                borderRadius: BorderRadius.circular(60),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Image(
+                  height: 40.0,
+                  width: 40.0,
+                  image: AssetImage('assets/filter_icon.png'),
                 ),
               ),
             ),
